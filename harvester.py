fc8257b20c373ad2f1f2c9b198704763273f7ece
Jeremy Prevost <jprevost@mit.edu>
2019-07-26 12:15:01 -0400

    rename to generic harvester

--- harvester.py
+++ harvester.py
@@ -23,75 +23,74 @@ import logging
 #  command line options from, until date format: YYYY-MM-DD
 #  TODO: add metadata types:   mods, qdc, oai_dc, mets, other?
 def get_parser():
-    # Get parser object 
+    # Get parser object
     # from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', help = "show help message and exit")
     parser.add_argument('-host', action='store',  dest='hostname', help='Set hostname', default="https://dspace.mit.edu")
-    parser.add_argument('-from', action='store',  dest='from_date', help='Set from date', default="2019-03-01") 
-    parser.add_argument('-until', action='store',  dest='until',  help='Until date', default="2019-06-03")   
-    parser.add_argument('-metadata-type', action='store',  dest='metadata-type',  help='Add metadata type (e.g. mods, mets, oai_dc, qdc)', default="mods") 
-    
+    parser.add_argument('-from', action='store',  dest='from_date', help='Set from date', default="2019-03-01")
+    parser.add_argument('-until', action='store',  dest='until',  help='Until date', default="2019-06-03")
+    parser.add_argument('-metadata-type', action='store',  dest='metadata-type',  help='Add metadata type (e.g. mods, mets, oai_dc, qdc)', default="mods")
+
     return parser
 
 
 def get_collection(handle):
-    
+
     logging.debug("Will get collection by handle URL")
 
 
 def list_collections(handle):
-    
+
     logging.debug("list all available collections")
-    
+
 
 def main():
-    
+
     # uri = URI('https://dspace.mit.edu/oai/request?verb=ListRecords&metadataPrefix=oai_dc')
-    
+
     counter = 0
-    
+
     logging.basicConfig(level=logging.DEBUGop)
     # logging.basicConfig(level=logging.INFO)
-    
+
     args = get_parser().parse_args()
-        
-    logging.debug("OAI-PMH testing on dspace.mit.edu") 
-    logging.debug("From date = %s", args.from_date)   
-    
+
+    logging.debug("OAI-PMH testing on dspace.mit.edu")
+    logging.debug("From date = %s", args.from_date)
+
     base_request =  args.hostname + '/oai/request'
-      
+
     # sickle = Sickle('http://dome.mit.edu/oai/request')
     # records = sickle.ListRecords(
         # **{'metadataPrefix': 'mets',
   #       'set': 'hdl_1721.3_45936'
   #       })
-        
-        
+
+
     mysickle = Sickle('https://dspace.mit.edu/oai/request', iterator=OAIItemIterator)
-    
+
     # mysickle = Sickle('https://dspace.mit.edu/oai/request')
-        
+
     # harvesting everything from dspace.mit.edu from the last 3 months
-    
+
     responses = mysickle.ListRecords(
         **{'metadataPrefix': 'mods',
         # 'set': 'hdl_1721.1_33972'
         'from': args.from_date
      })
-     
+
     # with open('/Users/carlj/Developer/Python/oai-pmh/response/dspace.mit.edu_ocw_from-2019-03-01_mods_response.xml', 'wb') as items:
     with open('/Users/carlj/Developer/Python/oai-pmh/dspace.mit.edu_everything_from-2019-03-01_mods_response.xml', 'wb') as items:
-        
+
         for records in responses:
             counter += 1
             if responses.resumption_token:
                items.write(records.raw.encode('utf8'))
                logging.debug(counter)
-                 
+
     logging.debug("Total records: %i", counter)
-    logging.debug("Done.")   
-    
+    logging.debug("Done.")
+
     exit(0)
 if __name__ == "__main__": main()
-
